dcf51e7d10e60f00f4c0e758b54962d6f02f0cdc
clkbug <mail@clkbug.net>
2024-06-08 22:07:26 +0900

    update

---
+++ docs/stat.R
@@ -0,0 +1,29 @@
+library(tidyverse)
+library(magrittr)
+library(plotly)
+library(htmlwidgets)
+
+data <- read_delim("data.txt", delim = " ", col_names = c("t", "value")) %>%
+  mutate(index = row_number())
+
+p0 <- data %>%
+  ggplot(mapping = aes(
+    x = index,
+    y = value
+  )) +
+  theme_bw() +
+  geom_point() +
+  scale_y_continuous(limits = c(0, NA))
+ggsave("plot.png", units = "px", width = 1600, height = 1200)
+
+p1 <- ggplotly(p0)
+save_image(p1, "plot.svg", width = 1600, height = 1200)
+
+p2 <- plot_ly(
+  data = data,
+  x = ~index,
+  y = ~value,
+  type = "scatter"
+)
+# save_image(p1, "plot2.svg", width = 1600, height = 1200)
+saveWidget(p2, "plot.html")
