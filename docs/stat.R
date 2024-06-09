06d4be31e3c1a90710757887f151d501166a766e
clkbug <mail@clkbug.net>
2024-06-09 14:18:01 +0900

    update

--- docs/stat.R
+++ docs/stat.R
@@ -2,9 +2,9 @@ library(tidyverse)
 library(magrittr)
 library(plotly)
 library(htmlwidgets)
-library(reticulate)
+# library(reticulate)
 
-use_python("/usr/bin/python")
+# use_python("/usr/bin/python")
 
 data <- read_delim("data.txt", delim = " ", col_names = c("t", "value")) %>%
   mutate(index = row_number())
@@ -20,7 +20,7 @@ p0 <- data %>%
 # ggsave("plot.png", units = "px", width = 1600, height = 1200)
 
 p1 <- ggplotly(p0)
-save_image(p1, "plot.svg", width = 1600, height = 1200)
+# save_image(p1, "plot.svg", width = 1600, height = 1200)
 
 p2 <- plot_ly(
   data = data,
